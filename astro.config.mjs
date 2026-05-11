b2751a9e16483f8ec108fed57307be40e1d08d40
Nergis <me@nergis.dev>
2026-05-11 22:54:56 +0900

    docs: format README table and update Cloudflare configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -44,8 +44,6 @@ export default defineConfig({
   },
 
   adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
+    imageService: { build: "compile", runtime: "cloudflare-binding" },
   }),
 });
